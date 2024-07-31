721db2d850b184a3c9f6263487205f6de43e14b6
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-07-30 22:06:06 -0300

    refactor: Improve snake avoidance logic in moveHandler

--- src/app/utils/snake_avoider.ts
+++ src/app/utils/snake_avoider.ts
@@ -5,15 +5,24 @@ type Snake = { body: Record<string, Point> };
 function snakeAvoider(availableDirections: Directions, allSnakes: any[]): Directions {
     const safeDirections: Directions = { ...availableDirections };
 
+    const movesToBeDeleted: string[] = [];
+
     for (const snk of allSnakes) {
       for (const move in safeDirections) {
-        if (safeDirections.hasOwnProperty(move)) {
-          if (snk.body.some((part: Point) => part.x === safeDirections[move].x && part.y === safeDirections[move].y)) {
-            delete safeDirections[move];
-          }
+        const getPosition = safeDirections[move];
+
+        if (getPosition.x === snk.body[0].x && getPosition.y === snk.body[0].y) {
+          movesToBeDeleted.push(move);
         }
       }
     }
+
+    // remove the moves that are duplicates in movesToBeDeleted
+    const remove = movesToBeDeleted.filter((item, index) => movesToBeDeleted.indexOf(item) === index);
+
+    remove.forEach(move => {
+      delete safeDirections[move];
+    });
 
     return safeDirections;
 }
